e07cbc13077a6d352e5711cad7bf3d9dd8669870
JaimeMartinezCuevas <jaimemarcudesigner@gmail.com>
2023-11-08 01:29:18 +0100

    First commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
  // Aquí tu código
+
+ const agregar = document.getElementById('agregar');
+ const lista = document.getElementById('lista');
+
+ agregar.addEventListener ('click', function() {
+
+    const nuevoElemento = prompt('Ingrese el nuevo elemento');
+    const nuevoLi = document.createElement('li');
+
+    nuevoLi.textContent = nuevoElemento;
+
+    lista.appendChild(nuevoLi);
+
+    });
